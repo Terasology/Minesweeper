0147840d52cb267778a86a7926e44e1a79927d3a
Michael Pollind <mpollind@gmail.com>
2021-02-25 12:35:59 -0800

    test: migrate test cases for jupiter (#22)

--- src/test/java/org/terasology/MineSweeper/generator/MineFieldTest.java
+++ src/test/java/org/terasology/MineSweeper/generator/MineFieldTest.java
@@ -16,13 +16,14 @@
 package org.terasology.MineSweeper.generator;
 
 import org.joml.Vector3i;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MineFieldTest {
     private Set<Vector3i> getNeighbors(Vector3i pos) {
@@ -44,34 +45,31 @@ public class MineFieldTest {
     public void getNumberOfNeighbors() throws Exception {
         MineField mineField = new MineField();
 
-        mineField.addMines(new Vector3i(0,0,0));
-        mineField.addMines(new Vector3i(0,1, 0));
-        mineField.addMines(new Vector3i(1,0,0));
-        mineField.addMines(new Vector3i(-1,0,0));
-        mineField.addMines(new Vector3i(0,-1,0));
-        mineField.addMines(new Vector3i(0,0,1));
-        mineField.addMines(new Vector3i(0,0,-1));
-        mineField.addMines(new Vector3i(1,1,1));
+        mineField.addMines(new Vector3i(0, 0, 0));
+        mineField.addMines(new Vector3i(0, 1, 0));
+        mineField.addMines(new Vector3i(1, 0, 0));
+        mineField.addMines(new Vector3i(-1, 0, 0));
+        mineField.addMines(new Vector3i(0, -1, 0));
+        mineField.addMines(new Vector3i(0, 0, 1));
+        mineField.addMines(new Vector3i(0, 0, -1));
+        mineField.addMines(new Vector3i(1, 1, 1));
 
-        assertEquals(8, mineField.getNumberOfNeighbors(new Vector3i(0,0,0)));
+        assertEquals(8, mineField.getNumberOfNeighbors(new Vector3i(0, 0, 0)));
     }
 
     @Test
     public void addMines() throws Exception {
         MineField mineField = new MineField();
 
-        for (Vector3i mine : getNeighbors(new Vector3i(1,1,1))) {
+        for (Vector3i mine : getNeighbors(new Vector3i(1, 1, 1))) {
             mineField.addMines(mine);
         }
-
-        for (Vector3i mine : getNeighbors(new Vector3i(0,0,0))) {
+        for (Vector3i mine : getNeighbors(new Vector3i(0, 0, 0))) {
             mineField.addMines(mine);
         }
-
-        for (Vector3i mine : getNeighbors(new Vector3i(0,0,1))) {
+        for (Vector3i mine : getNeighbors(new Vector3i(0, 0, 1))) {
             mineField.addMines(mine);
         }
-
         assertTrue(mineField.getMines().stream().allMatch(mine -> mineField.getNumberOfNeighbors(mine) <= 16));
     }
 }
